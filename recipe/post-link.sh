c94079e3ce9e96b88debe09b1b0691e5043b42b0
Philippe Hupé <philippe.hupe@curie.fr>
2022-06-29 13:46:51 +0200

    [MODIF] copy specific unsquashfs/mksquashfs for apptainer

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -xeuf
 
+cp ${PREFIX}/bin/unsquashfs ${PREFIX}/bin/unsquashfs4apptainer
+cp ${PREFIX}/bin/mksquashfs ${PREFIX}/bin/mksquashfs4apptainer
+
 # See https://github.com/apptainer/apptainer/issues/519
 # this patch avoids having relative PATh to the shared libraries
 # needed by unsquasfs
-patchelf --set-rpath ${PREFIX}/lib ${PREFIX}/bin/unsquashfs
-patchelf --set-rpath ${PREFIX}/lib ${PREFIX}/bin/mksquashfs
+patchelf --set-rpath ${PREFIX}/lib ${PREFIX}/bin/unsquashfs4apptainer
+patchelf --set-rpath ${PREFIX}/lib ${PREFIX}/bin/mksquashfs4apptainer
+
+
+echo "mksquashfs path = ${PREFIX}/bin/mksquashfs4apptainer" >> ${PREFIX}/etc/apptainer/apptainer.conf
+echo "unsquashfs path = ${PREFIX}/bin/unsquashfs4apptainer" >> ${PREFIX}/etc/apptainer/apptainer.conf
+
