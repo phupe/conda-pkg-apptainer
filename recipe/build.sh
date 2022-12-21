e7d2f604c4389ae4e6647d8770700dc21ed2a988
Philippe Hupé <philippe.hupe@curie.fr>
2022-12-21 14:30:33 +0100

    [MODIF] Update the build scripts (Issue #5)

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,26 +7,22 @@ if [[ $(uname -m) == "ppc64le" ]]; then
     mkdir -p "${GOPATH}"
 fi
 
-echo $CPPFLAGS
-
 pushd src/github.com/apptainer/${PKG_NAME}
+
+# The "starter" binary inherits the stack from "singularity" meaning FORTIFY_SOURCE cannot be used
+CGO_CPPFLAGS=$(echo "${CGO_CPPFLAGS}" | sed -E 's@FORTIFY_SOURCE=[0-9]@FORTIFY_SOURCE=0@g')
+export CGO_CPPFLAGS
 
 # configure
 ./mconfig \
   -P release-stripped \
   --without-suid \
+  -V "${PKG_VERSION}" \
   -p "${PREFIX}" \
   -c "${CC}" \
   -x "${CXX}"
 
-go env
-
-# The "starter" binary inherits the stack from "singularity" meaning FORTIFY_SOURCE cannot be used
-#CGO_CPPFLAGS=$(echo "${CGO_CPPFLAGS}" | sed -E 's@FORTIFY_SOURCE=[0-9]@FORTIFY_SOURCE=0@g')
-#export CGO_CPPFLAGS
-
 # build
-
 pushd builddir
 export LD_LIBRARY_PATH=${PREFIX}/lib
 make
